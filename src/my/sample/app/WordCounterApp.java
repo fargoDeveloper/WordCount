e957fea10a5c96a967412d1edbbf82f788e90913
MNedoshovenko <f.test.05.2021@gmail.com>
2021-07-12 16:53:21 +0300

    Fixed bug. Now all working

--- src/my/sample/app/WordCounterApp.java
+++ src/my/sample/app/WordCounterApp.java
@@ -13,26 +13,27 @@ public class WordCounterApp {
         FromFileReader fromFileReader = new FromFileReader();
         LineConverter lineConverter = new LineConverter();
         DuplicateCounter duplicateCounter = new DuplicateCounter();
-        HashMap<String, Integer> wordCountMap = new HashMap<>();
+        HashMap<String, Integer> wordCountMap1 = new HashMap<>();
 
         List<String> linesFromFile = fromFileReader.loadFromFile(fileName);
 
         for (String line : linesFromFile) {
             if (!line.isEmpty()) {
                 List<String> words = lineConverter.extractWords(line);
-                duplicateCounter.countWords(words, wordCountMap);
+                duplicateCounter.countWords(words, wordCountMap1);
+                words.clear();
             }
         }
 
         System.out.println();
-        System.out.println("List of duplicates and their number: " + wordCountMap);
+        System.out.println("List of duplicates and their number: " + wordCountMap1);
         System.out.println();
-        System.out.println("There are " + wordCountMap.size() + " words in the text.");
+        System.out.println("There are " + wordCountMap1.size() + " words in the text.");
 
         System.out.println();
         System.out.println("------------------------ Sorted option ------------------------");
 
-        Set<Map.Entry<String, Integer>> setWords = wordCountMap.entrySet();
+        Set<Map.Entry<String, Integer>> setWords = wordCountMap1.entrySet();
         List<Map.Entry<String, Integer>> listWords = new ArrayList<>(setWords);
         listWords.sort(Map.Entry.comparingByValue());
 
